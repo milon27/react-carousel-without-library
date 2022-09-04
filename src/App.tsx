6caa92e624ae5b06ad1abd4d7d181e8d1cff1d04
milon27 <mdjahidulislammilon@gmail.com>
2022-09-04 14:00:37 +0600

    add unlimited images

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,43 @@ import { ImageList } from "./slider/ImageList"
 
 
 export default function App() {
+  const test = false
   const [select, setSelect] = useState(0)
+
+  const prev = () => {
+    setSelect(old => {
+      if (old == 5) {
+        return 2
+      }
+      return old - 1
+    })
+  }
+
+  const next = () => {
+    setSelect(old => {
+      if (old == ImageList.length + 1) {
+        return 2
+      }
+      return old + 1
+    })
+  }
 
   return (
     <div className="grid place-content-center min-h-screen">
-
-      {/* <h1 className="text-4xl text-center">{select}</h1> */}
+      {/* <h1 className="text-4xl text-center">{"select=" + select}</h1> */}
       {/* <h1 className="text-4xl text-center">{select - 1}</h1> */}
 
-
-      <div className="ccontainer w-[50%] mx-auto overflow-hidden">
+      <div className={`cContainer  mx-auto ${test ? "w-[15%] border-2 border-red-500" : "w-[50%] overflow-hidden"}`}>
 
         <div className="clist flex w-full h-96 " >
 
           {ImageList.map((item, idx) => (
-            <img src={item} alt="" className="grow-0 shrink-0 w-full object-cover transition-transform" key={idx}
+            <img src={item} alt={"id-" + idx} className="grow-0 shrink-0 w-full object-cover transition-transform" key={idx}
               style={{
-                "transform": select == 5 ? 'translateX(-100%)' : `translateX(${(idx < select - 1) ? (((ImageList.length) - select) * 100) : select * (-100)}%)`,
+                "transform": select == (ImageList.length + 1) ?
+                  'translateX(-100%)'
+                  :
+                  `translateX(${(idx < select - 1) ? ((ImageList.length - select) * 100) : select * (-100)}%)`,
               }}
             />
           ))}
@@ -27,23 +47,13 @@ export default function App() {
         </div>
 
       </div>
-
       {/* indicator */}
-
       <div className="flex gap-4 justify-center items-center">
-        <button className="border py-2.5 px-5 text-sm text-gray-500" onClick={() => {
-          setSelect(old => {
-            if (old == 5) {
-              return 2
-            }
-            return old + 1
-          })
-        }}>prev</button>
-
+        <button className="border py-2.5 px-5 text-sm text-gray-500" onClick={prev}>prev</button>
         <div className="flex gap-2 justify-center my-5">
           {ImageList.map((item, idx) => (
             <img src={item} alt=""
-              className={`w-[100px] cursor-pointer object-cover ${(idx == ((select > 3) ? (select - 4) : select)) ? "border-2 border-black" : ""}`}
+              className={`w-[100px] cursor-pointer object-cover ${(idx == ((select > ImageList.length - 1) ? (select - ImageList.length) : select)) ? "border-2 border-black" : ""}`}
               key={idx}
               onClick={() => {
                 setSelect(idx)
@@ -51,18 +61,8 @@ export default function App() {
             />
           ))}
         </div>
-
-        <button className="border py-2.5 px-5 text-sm text-gray-500" onClick={() => {
-          setSelect(old => {
-            if (old == 5) {
-              return 2
-            }
-            return old + 1
-          })
-        }}>next</button>
+        <button className="border py-2.5 px-5 text-sm text-gray-500" onClick={next}>next</button>
       </div>
-
-
     </div>
   )
 }
--- src/slider/ImageList.ts
+++ src/slider/ImageList.ts
@@ -3,5 +3,5 @@ export const ImageList = [
     "https://i.picsum.photos/id/866/536/354.jpg?hmac=tGofDTV7tl2rprappPzKFiZ9vDh5MKj39oa2D--gqhA",
     "https://i.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0",
     "https://i.picsum.photos/id/1084/536/354.jpg?grayscale&hmac=Ux7nzg19e1q35mlUVZjhCLxqkR30cC-CarVg-nlIf60",
-    // "https://i.picsum.photos/id/1060/536/354.jpg?blur=2&hmac=0zJLs1ar00sBbW5Ahd_4zA6pgZqCVavwuHToO6VtcYY",
+    "https://i.picsum.photos/id/1060/536/354.jpg?blur=2&hmac=0zJLs1ar00sBbW5Ahd_4zA6pgZqCVavwuHToO6VtcYY",
 ]
